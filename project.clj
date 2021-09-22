08411668e0de436af2df1573a66c0562231e2d4e
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-09-22 13:22:29 +0200

    Prepare for deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tiny-rbac "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.flexiana/tiny-rbac "0.1.0"
+  :description "Role based access control"
+  :url "https://github.com/Flexiana/tiny-rbac"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
